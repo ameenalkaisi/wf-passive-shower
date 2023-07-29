67247d443f89b81b618578d7118193ead04a5bb6
Ameen Al-Kaisi <ameen.alkaisi@borderconnect.com>
2023-07-28 22:04:19 -0400

    working on backend seeing how it will be modeled

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,5 +1,27 @@
+use rocket::request::FromRequest;
+
 #[macro_use]
 extern crate rocket;
+
+// use tempfile form????
+struct WfScreenshot {
+    // image object here
+}
+
+struct Passive {
+    // image correlating to it maybe
+    desc: String,
+
+    // instead of string should be some URL object
+    wiki_link: String,
+}
+
+// note: should return array of results of names maybe
+// or objects giving more info on each of the passives found
+#[post("/find-passives-by-screenshot")]
+fn find_image<'r>(screenshot: &'r WfScreenshot) -> &'static str {
+    "todo"
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +30,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, find_image])
 }
