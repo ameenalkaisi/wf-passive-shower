3351f558318568a3bae14fad44461bfbdad4be51
Ameen Al-Kaisi <ameen.alkaisi@borderconnect.com>
2023-08-01 18:35:27 -0400

    working on image upload testing

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,11 +1,16 @@
-use rocket::request::FromRequest;
+use rocket::{form::Form, fs::TempFile};
 
 #[macro_use]
 extern crate rocket;
 
 // use tempfile form????
-struct WfScreenshot {
+// note: should accept ANY image type
+// might depend on the tool used to do the pattern matching instead of at the data layer
+#[derive(FromForm)]
+struct WfScreenshot<'r> {
     // image object here
+    #[field(name = "image")]
+    img: TempFile<'r>,
 }
 
 struct Passive {
@@ -18,9 +23,10 @@ struct Passive {
 
 // note: should return array of results of names maybe
 // or objects giving more info on each of the passives found
-#[post("/find-passives-by-screenshot")]
-fn find_image<'r>(screenshot: &'r WfScreenshot) -> &'static str {
-    "todo"
+#[post("/find-passives-from-ss", data = "<form>")]
+async fn upload_image(mut form: Form<WfScreenshot<'_>>) -> std::io::Result<()> {
+    form.img.persist_to("/tmp/complete/file.png").await?;
+    Ok(())
 }
 
 #[get("/")]
@@ -30,5 +36,38 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, find_image])
+    println!(env!("CARGO_MANIFEST_DIR"));
+    rocket::build().mount("/", routes![index, upload_image])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::rocket;
+    use rocket::{
+        http::{ContentType, Status},
+        local::blocking::Client,
+    };
+    use std::{
+        fs::File,
+        io::{BufReader, Read},
+    };
+
+    #[test]
+    fn file_is_saved() {
+        let mut img: String = "";
+        let reader = BufReader::new(File::open("test/first.png").unwrap());
+
+        let mut client = Client::tracked(rocket()).unwrap();
+
+        let mut req = client
+            .post(uri!(super::upload_image))
+            .header(ContentType::FormData)
+            // up to here correct
+            //
+            // need to do special things here
+            .body(img);
+
+        let response = req.dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
 }
