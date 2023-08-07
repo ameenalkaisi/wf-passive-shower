fae5daf72202944018879505965340bf6df2f89a
Ameen Al-Kaisi <ameen.alkaisi@borderconnect.com>
2023-08-07 19:57:57 -0400

    finished unit testing of uploading an image

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -29,11 +29,6 @@ async fn upload_image(mut form: Form<IncomingWfScreenshot<'_>>) -> std::io::Resu
     Ok(())
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index, upload_image])
@@ -41,30 +36,99 @@ fn rocket() -> _ {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        env,
+        io::{self, Read, Write},
+        path::PathBuf,
+    };
+
     use super::rocket;
+    use image::RgbImage;
     use rocket::{
-        http::{ContentType, Status},
+        http::{Header, Status},
         local::blocking::Client,
     };
+    use std::fs::File;
+
+    // https://stackoverflow.com/questions/66921683/rocket-testing-multipart-data-form-fails-with-422-unprocessable-entity
+    // used above for image uploading functionality
+    fn create_image(file_name: &str) -> PathBuf {
+        let path_buf = env::temp_dir().join(file_name);
+
+        let imgbuf: RgbImage = image::ImageBuffer::new(256, 256);
+        imgbuf.save(path_buf.as_path()).unwrap();
+
+        path_buf
+    }
+
+    // creates image data section, to be used in multipart send
+    fn image_data(boundary: &str, image_path: PathBuf) -> io::Result<Vec<u8>> {
+        // https://stackoverflow.com/questions/51397872/how-to-post-an-image-using-multipart-form-data-with-hyper
+        // https://golangbyexample.com/multipart-form-data-content-type-golang/
+        let mut data = Vec::new();
+
+        // start
+        write!(data, "--{}\r\n", boundary)?;
+
+        // image data
+        write!(
+            data,
+            "Content-Disposition: form-data; name=\"image\"; filename=\"image.jpg\"\r\n"
+        )?;
+        write!(data, "Content-Type: image/jpeg\r\n")?;
+        write!(data, "\r\n")?;
+
+        // let path_buf = create_image("image.jpg");
+        let path_buf = image_path;
+
+        let mut f = File::open(path_buf.as_path())?;
+        f.read_to_end(&mut data)?;
+
+        write!(data, "\r\n")?;
+        write!(data, "--{}\r\n", boundary)?;
+
+        Ok(data)
+    }
+
+    #[test]
+    fn upload_works_with_random_image() -> anyhow::Result<()> {
+        const BOUNDARY: &str = "--------------------------------XYZ";
+
+        let client = Client::tracked(rocket()).unwrap();
+
+        let content_type = Header::new(
+            "Content-Type",
+            format!("multipart/form-data; boundary={}", BOUNDARY),
+        );
+
+        let response = client
+            .post(uri!(super::upload_image))
+            .header(content_type)
+            .body(image_data(BOUNDARY, create_image("image.jpg")).unwrap())
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+
+        Ok(())
+    }
 
     #[test]
     fn file_is_saved() -> anyhow::Result<()> {
-        let img = image::io::Reader::open("myimage.png")?.decode().unwrap();
-        // let reader = BufReader::open(File::open("test/first.png").unwrap()).;
+        const BOUNDARY: &str = "--------------------------------XYZ";
 
-        let mut client = Client::tracked(rocket()).unwrap();
+        let client = Client::tracked(rocket()).unwrap();
 
-        let mut req = client
+        let content_type = Header::new(
+            "Content-Type",
+            format!("multipart/form-data; boundary={}", BOUNDARY),
+        );
+
+        let response = client
             .post(uri!(super::upload_image))
-            .header(ContentType::FormData)
-            // up to here correct
-            //
-            // need to do special things here
-            //
-            // something to do with boundaries not sure
-            .body(img);
+            .header(content_type)
+            .body(image_data(BOUNDARY, PathBuf::from("test/first.png")).unwrap())
+            .dispatch();
 
-        let response = req.dispatch();
         assert_eq!(response.status(), Status::Ok);
 
         Ok(())
